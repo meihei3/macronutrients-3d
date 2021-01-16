aa87b906ae0092de33ab4fb961ec37938af04326
yameholo <ame.aroid.xz5@gmail.com>
2021-01-16 19:52:53 +0900

    Add sample code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,35 @@
-import * as d3 from 'd3';
+const trace1 = {
+  text: ['a', 'b', 'c', 'd', 'e'],
+  x: [5, 3, 8, 9, 4],
+  y: [1.2, 7.2, 4.2, 3.2, 8.2],
+  z: [5.2, 3.4, 8.1, 9.2, 2.4],
+  mode: 'markers',
+  marker: {
+    size: 8,
+    color: 'rgba(17, 217, 217, 0.7)',
+    line: {
+      color: 'rgb(77, 77, 250)',
+      width: 3,
+    },
+  },
+  type: 'scatter3d',
+};
 
-d3.select('#app').append('p').text('new text');
+const data = [trace1];
+
+const layout = {
+  title: 'Macronutrients',
+  scene: {
+    xaxis: {
+      title: 'タンパク質',
+    },
+    yaxis: {
+      title: '炭水化物',
+    },
+    zaxis: {
+      title: '脂質',
+    },
+  },
+};
+
+Plotly.newPlot('app', data, layout);
